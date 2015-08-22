ed9c5c92f3c8c7c89ce5a2bbba8ad9684f54855a
David A Koch <kodave1@gmail.com>
2015-08-21 21:30:15 -0400

    added sctwo plugin

--- scripts/main.js
+++ scripts/main.js
@@ -5,4 +5,12 @@ $(document).on('ready', function(){
 
 	});
 
+
+	//this scrolls slowly to a section of the page
+	$('.navMenu a').on('click', function(){
+		var href = $(this).attr('href');
+		$.scrollTo(href, 800);
+	});
+
+
 });//end of document on ready
