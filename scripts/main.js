86a8b8f96bce414a383afa697500bdef65e8eeb9
David A Koch <kodave1@gmail.com>
2015-08-25 21:35:12 -0400

    added section separators

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 $(document).on('ready', function(){
 
+	//makes the navMenu appear when click on hamburger
 	$('.js-menu').on('click', function(){
 		$('.navMenu').slideToggle();
 
 	});
 
+	//this makes the navMenu reappear after it's been lost
 	$(window).on('resize', function(){
 		var w = $(window).width(); 
 		if (w > 760 && $('.navMenu').is(':hidden')){
@@ -24,7 +26,7 @@ $(document).on('ready', function(){
 	$(window).on('scroll', function(){
 		var topWin = $(window).scrollTop();
 		//console.log(topWin);
-		if(topWin >= 300 && $('nav-wrapper').css('position') !== 'fixed'){
+		if(topWin >= 300 && $('.nav-wrapper').css('position') !== 'fixed'){
 			$('.nav-wrapper').hide().fadeIn().css('position', 'fixed')
 		}	else if (topWin < 300){
 				$('.nav-wrapper').css('position', 'static');
@@ -32,6 +34,8 @@ $(document).on('ready', function(){
 		}
 
 	});
+
+
 
 
 });//end of document on ready
