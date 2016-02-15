865a99dfd7ff1044fcbd3d147fbb9842a1a9ce72
David A Koch <kodave1@gmail.com>
2016-02-15 17:16:03 -0500

    added wysong music

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,5 @@
 $(document).on('ready', function(){
 
-	//makes the navMenu appear when click on hamburger
-	$('.js-menu').on('click', function(){
-		$('.navMenu').slideToggle();
-
-	});
-
-	//this makes the navMenu reappear after it's been lost
-	$(window).on('resize', function(){
-		var w = $(window).width(); 
-		if (w > 760 && $('.navMenu').is(':hidden')){
-			$('.navMenu').removeAttr('style'); 
-		}
-	}); 
 
 
 	//this scrolls slowly to a section of the page
@@ -27,6 +14,8 @@ $(document).on('ready', function(){
 		$('.navMenu a').css('text-decoration', 'none');
 		$(this).css('text-decoration', 'underline');
 	});
+
+
 
 
 
