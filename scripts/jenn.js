cb21431ff2cf5d7e106d9eb852535450744fc8d0
David A Koch <kodave1@gmail.com>
2017-06-25 23:31:14 -0400

    added some opening code to jenn js

--- scripts/jenn.js
+++ scripts/jenn.js
@@ -1,3 +1,11 @@
+var modal = function(state){
+	if(state === 'open'){
+		$('.modal-wrapper').fadeIn(function(){
+			$('body').addClass('modal-on');
+		});
+	}
+}
+
 $(document).on('ready', function(){
 
 	$('.front_image').on('click', function(){
