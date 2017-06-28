493cc446ff5ecd2d298f56445902829e98ff9c69
David A Koch <kodave1@gmail.com>
2017-06-27 22:23:10 -0400

    fixed up the jquery script

--- scripts/jenn.js
+++ scripts/jenn.js
@@ -1,20 +1,26 @@
-var modal = function(state){
-	if(state === 'open'){
-		$('.modal-wrapper').fadeIn(function(){
-			$('body').addClass('modal');
-		});
-	} else {
-		$('.modal-wrapper').fadeOut(function(){
-			$('body').removeClass('modal');
-		});
-	}
-}
+
 
 $(document).on('ready', function(){
 
+
+	/*var modal = function(state){
+		if(state === 'open'){
+			//$('.modal-wrapper').fadeIn(function(){
+				//$('body').addClass('modal');
+			$('.modal-wrapper').css({display:'block'});
+			//};
+		} else {
+			//$('.modal-wrapper').fadeOut(function(){
+				//$('body').removeClass('modal');
+			$('.modal-wrapper').css({display:'none'});
+			//};
+		}
+	}*/
+
 	$('.front_image').on('click', function(){
 		console.log("hello");
-		modal('open');
+		//modal('open');
+		$('.modal_wrapper').css("display", "block");
 	});
 
 	$('.js-modal-close').on('click', function(){
