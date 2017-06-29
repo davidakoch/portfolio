8c1b330691f6899e542286eeb2fb29b1cf13a884
David A Koch <kodave1@gmail.com>
2017-06-28 21:43:21 -0400

    added on click events to images

--- scripts/jenn.js
+++ scripts/jenn.js
@@ -3,21 +3,19 @@
 $(document).on('ready', function(){
 
 
-	/*var modal = function(state){
-		if(state === 'open'){
-			//$('.modal-wrapper').fadeIn(function(){
-				//$('body').addClass('modal');
-			$('.modal-wrapper').css({display:'block'});
-			//};
-		} else {
-			//$('.modal-wrapper').fadeOut(function(){
-				//$('body').removeClass('modal');
-			$('.modal-wrapper').css({display:'none'});
-			//};
-		}
-	}*/
+	$('.front_image').on('click', function(){
+		console.log("hello");
+		//modal('open');
+		$('.modal_wrapper').css("display", "block");
+	});
 
-	$('.front_image, .news_image').on('click', function(){
+	$('.issues_image').on('click', function(){
+		console.log("hello");
+		//modal('open');
+		$('.modal_wrapper').css("display", "block");
+	});
+
+	$('.news_image').on('click', function(){
 		console.log("hello");
 		//modal('open');
 		$('.modal_wrapper').css("display", "block");
